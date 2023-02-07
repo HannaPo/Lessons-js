365159f46d34352614e595d0f25142c6ce25fd65
GoItTutor <sheva09a@gmail.com>
2023-02-07 21:34:39 +0200

    module-05-lesson-01

--- index.js
+++ index.js
@@ -28,17 +28,27 @@ const user1 = {
   },
 };
 
+// user1.showThis();
+
+// user1.sayHi();
+
 const user2 = {
   name: 'Andrii',
   age: 30,
   city: 'Lviv',
 
-  // showThis: user1.showThis
+  showThis: user1.showThis,
 
   // sayHi: user1.sayHi
 
-  // sayFromWhere: user1.sayFromWhere
+  sayFromWhere: user1.sayFromWhere,
 };
+
+// user2.sayFromWhere();
+
+// const func = user1.showThis;
+
+// func();
 
 /*
   2. Значення this залежно від місця звернення
@@ -53,7 +63,7 @@ const user2 = {
 // this у глобальній області видимості
 
 // function showThis() {
-//   console.log(this);
+//   console.log(this.name);
 // }
 
 // showThis();
@@ -68,6 +78,8 @@ const user2 = {
 //     return `${this.firstName} ${this.lastName}`;
 //   },
 // };
+
+// console.log(user.getFullName());
 
 // function makeInvoice(callback) {
 //   console.log(`Обробляємо заявку від ${callback()}.`);
@@ -97,9 +109,37 @@ const user2 = {
 
 //     arrowFunc();
 //   },
+
+//   showThis: () => {
+//     console.log(this);
+//   },
 // };
 
 // data.showDate();
+
+// data.showThis();
+
+// const newObj = {
+//   name: 'Galyna',
+//   showThis: function () {
+//     const func = () => {
+//       console.log(this);
+//     };
+
+//     func();
+//   },
+// };
+
+// const newObj2 = {
+//   name: 'Vitaliy',
+//   showThis: newObj.showThis,
+// };
+
+// newObj2.showThis();
+
+// newObj.showThis();
+
+// newObj.showThis();
 
 /*
   3. Методи функцій bind, call, apply
@@ -107,19 +147,30 @@ const user2 = {
 
 // Метод call
 
-// function getData() {
-//   console.log(`${this.username} is ${this.age} years old.`);
+// function getData(room) {
+//   console.log(
+//     `${this.username} is ${this.age} years old. Your room number is ${room}`
+//   );
 // }
+
+// // console.dir(getData);
 
 // const userFirst = {
 //   username: 'Max',
 //   age: 29,
+//   // getData,
 // };
+
+// getData.call(userFirst, 55);
+
+// userFirst.getData();
 
 // const userSecond = {
 //   username: 'Anatoliy',
 //   age: 33,
 // };
+
+// getData.call(userSecond, 618);
 
 // Метод apply
 
@@ -132,10 +183,22 @@ const user2 = {
 //   age: 30,
 // };
 
+// greetGuest.apply(firstUser, ['Hello']);
+
 // const secondUser = {
 //   username: 'Vitaliy',
 //   age: 35,
 // };
+
+// greetGuest.apply(secondUser, ['Hi']);
+
+// function func(...rest) {
+//   return Math.max.call(null, ...rest);
+// }
+
+// const result = func(500, 1000, -5);
+
+// console.log(result);
 
 // Метод bind
 
@@ -154,9 +217,11 @@ const user2 = {
 //   mark: 'Bogdan',
 // };
 
-// const getInfo = fordAuto.getInfo;
+// const getInfo = fordAuto.getInfo.bind(fordAuto).bind(bogdanAuto);
 
-// getInfo()
+// console.log(getInfo === fordAuto.getInfo);
+
+// getInfo.call(bogdanAuto);
 
 /*
   4. Калькулятор
@@ -171,7 +236,28 @@ const user2 = {
 // const calculator = {
 //   a: 0,
 //   b: 0,
+
+//   write(a, b) {
+//     this.a = a;
+//     this.b = b;
+//   },
+
+//   getSum() {
+//     return this.a + this.b;
+//   },
+
+//   getMulti() {
+//     return this.a * this.b;
+//   },
 // };
+
+// calculator.write(50, 100);
+
+// console.log(calculator.getSum());
+
+// console.log(calculator.getMulti());
+
+// console.log(calculator);
 
 /*
   5. Це ladder (драбина) – об'єкт, який дозволяє підніматись вгору
@@ -184,9 +270,38 @@ const user2 = {
   showStep - показати сходинку
 */
 
-// const ladder = {
-//   step: 0,
-// };
+const ladder = {
+  step: 0,
+
+  up() {
+    this.step += 1;
+
+    return this;
+  },
+
+  down() {
+    if (this.step === 0) {
+      console.log('знаходишся на 0 сходинці');
+      return this;
+    }
+
+    this.step -= 1;
+
+    return this;
+  },
+
+  showStep() {
+    console.log(this.step);
+  },
+};
+
+// ladder.up();
+// ladder.up();
+// ladder.up();
+
+// ladder.down();
+
+// ladder.showStep();
 
 /* 
   Змініть код методів up, down та showStep таким чином, щоб 
@@ -194,6 +309,8 @@ const user2 = {
 
   ladder.up().up().down().showStep()
 */
+
+// ladder.up().up().down().showStep();
 
 /*
   6. Напишіть метод calcTotalPrice(stoneName), який приймає назву
@@ -208,7 +325,55 @@ const user2 = {
 //     { name: 'Sapphire', price: 1400, quantity: 7 },
 //     { name: 'Ruby', price: 800, quantity: 2 },
 //   ],
+
+//   calcTotalPrice(stoneName) {
+//     const stoneObj = this.stones.find(
+//       ({ name }) => name.toLowerCase() === stoneName.trim().toLowerCase()
+//     );
+
+//     if (!stoneObj) {
+//       return `Каміння з назвою ${stoneName} не знайшлося`;
+//     }
+
+//     return stoneObj.price * stoneObj.quantity;
+//   },
 // };
 
-// console.log(chopShop.calcTotalPrice('Emerald')); // 5200
+// // console.log(chopShop.calcTotalPrice('   EmErаіваіввіaLd   ')); // 5200
 // console.log(chopShop.calcTotalPrice('Diamond')); // 8100
+
+// const showThis = function () {
+//   console.log(`This show -> this`, this);
+// };
+
+// const ObjA = {
+//   a: 5,
+//   b: 10,
+// };
+
+// ObjA.showThis = showThis;
+
+// ObjA.showThis();
+
+// showThis.call(ObjA);
+
+// function showProps({ eyeColor, gender, age }) {
+//   console.log(eyeColor, gender, age);
+// }
+
+// showProps({ eyeColor: 'blue', age: 25, gender: 'female' })
+
+// showProps.call(null, { eyeColor: 'blue', age: 25, gender: 'female' });
+
+// const players = [
+//   { name: 'Mango', playtime: 1270, gamesPlayed: 4 },
+//   { name: 'Poly', playtime: 469, gamesPlayed: 2 },
+//   { name: 'Ajax', playtime: 690, gamesPlayed: 3 },
+//   { name: 'Kiwi', playtime: 241, gamesPlayed: 1 },
+// ];
+
+// debugger;
+
+// const totalAveragePlaytimePerGame = players
+//   .map((player) => player.playtime / player.gamesPlayed)
+//   .reduce((previousTime, time) => previousTime + time, 0);

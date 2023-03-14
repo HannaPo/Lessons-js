80d6acff5dc0870271db2359d077ef9d55e2f200
GoItTutor <sheva09a@gmail.com>
2023-03-14 20:36:37 +0100

    module-09-lesson-01

--- src/js/async.js
+++ src/js/async.js
@@ -3,9 +3,52 @@
  * Синхронний vs Асинхронний JS
  */
 
+// const greet = () => {
+//   console.log('Hello, User!');
+// };
+
+// setTimeout(() => {
+//   console.log('Hello from setTimout 2000ms');
+// }, 2000);
+
+// setTimeout(() => {
+//   console.log('Hello from setTimout 1500ms');
+// }, 1500);
+
+// console.log('First');
+
+// // let counter = 0;
+
+// // for (let i = 0; i < 1000000000; i += 1) {
+// //   counter += i;
+// // }
+
+// greet();
+
+// console.log('Second');
+
+// const timerId = setInterval(() => {
+//   console.log('HI!');
+// }, 1000);
+
+// console.log(timerId);
+
+//   clearInterval(timerId);
+
+// console.log('second');
+
 // Стек виклику функцій
 // http://latentflip.com/loupe/?code=Y29uc29sZS5sb2coJ2ZpcnN0Jyk7CgpzZXRUaW1lb3V0KGZ1bmN0aW9uKCkgewogICAgY29uc29sZS5sb2coJ2lubmVyIHNldFRpbWVvdXQnKTsKfSwgMTAwMCk7Cgpjb25zb2xlLmxvZygnc2Vjb25kJyk7!!!
 
 // Функції відкладеного виклику window.setTimeout(callback, delay, arg1, arg2, ...);
 
 // Очищення інтервалів і таймаутів clearInterval(intervalID), clearTimeout(timeoutID)
+
+// setTimeout(() => {
+//   console.log(300);
+//   clearTimeout(id);
+// }, 300);
+
+// const id = setTimeout(() => {
+//   console.log(500);
+// }, 500);
--- src/js/date.js
+++ src/js/date.js
@@ -6,9 +6,25 @@
 //? TASK 01
 // Створіть об'єкт Date для дати: 20 лютого 2012 року, 3 години 12 хвилин.
 
+// console.log(new Date(2012, 1, 20, 3, 12));
+
 //? TASK 02
 // Створіть функцію getWeekDay(date), яка показує день тижня у короткому форматі:
 // «ПН», «ВТ», «СР», «ЧТ», «ПТ», «СБ», «НД».
+
+// const WEEK_DAYS = ['НД', 'ПН', 'ВТ', 'СР', 'ЧТ', 'ПТ', 'CБ'];
+
+// const currentDate = new Date();
+
+// currentDate.setDate(15);
+
+// const getWeekDay = date => {
+//   const dayIndex = date.getDay();
+
+//   return WEEK_DAYS[dayIndex];
+// };
+
+// console.log(getWeekDay(currentDate));
 
 //? TASK 03
 // Створіть функцію getLastDayOfMonth(year, month), яка повертає останнє число місяця.
@@ -18,5 +34,29 @@
 // month – місяць від 0 до 11.
 // Наприклад, getLastDayOfMonth(2012, 1) = 29 (високосний рік, лютий).
 
+// const getLastDayOfMonth = (year, month) => {
+//   const date = new Date(year, month + 1, 0);
+
+//   return date.getDate();
+// };
+
+// console.log(getLastDayOfMonth(2012, 1));
+
 //? TASK 04
 // Створіть функцію getSecondsToTomorrow(), яка повертає кількість секунд до завтрашнього дня.
+
+// const getSecondsToTomorrow = () => {
+//   const date = new Date();
+
+//   const tomorrowDate = new Date(
+//     date.getFullYear(),
+//     date.getMonth(),
+//     date.getDate() + 1
+//   );
+
+//   return Math.trunc((tomorrowDate - date) / 1000);
+// };
+
+// setInterval(() => {
+//   console.log(getSecondsToTomorrow());
+// }, 1000);

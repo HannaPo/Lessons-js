72ada5d61769429c46d9aa98694cc0bb2fdd14b9
GoItTutor <sheva09a@gmail.com>
2023-03-28 20:17:27 +0200

    module-11-lesson-01

--- src/js/put-patch.js
+++ src/js/put-patch.js
@@ -4,14 +4,32 @@
  * Method(PUT/PATCH). Headers ("Content-Type": "application/json"). Body.
  */
 
+import axios from 'axios';
+
 const BASE_URL = 'http://localhost:3000';
 
-// const updateUserById = (userId, infoForUpdate) => {}
+// const updateUserById = (userId, infoForUpdate) =>
+//   fetch(`${BASE_URL}/users/${userId}`, {
+//     method: 'PATCH',
+//     body: JSON.stringify(infoForUpdate),
+//     headers: {
+//       'Content-Type': 'application/json',
+//     },
+//   }).then(res => {
+//     if (!res.ok) {
+//       throw new Error(res.status);
+//     }
 
-// updateUserById(15, { name: 'Oleksii Repin', email: 'test@gmail.com' })
-//   .then(data => {
-//     console.log(data);
-//   })
-//   .catch(err => {
-//     console.log(err);
+//     return res.json();
 //   });
+
+const updateUserById = (userId, infoForUpdate) =>
+  axios.put(`${BASE_URL}/users/${userId}`, infoForUpdate);
+
+updateUserById(9, { name: 'Andrii Shevchuk', email: 'test422242@gmail.com' })
+  .then(data => {
+    console.log(data);
+  })
+  .catch(err => {
+    console.log(err);
+  });

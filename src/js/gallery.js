efc205bc46583abc15eb10dfe9dfcee637f45b71
GoItTutor <sheva09a@gmail.com>
2023-03-28 17:31:49 +0200

    module-11-lesson-01

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,60 +1,57 @@
 'use strict';
 // https://unsplash.com/
-
 import { UnsplashAPI } from './unsplash-api';
-
 import createGalleryCards from '../templates/gallery-card.hbs';
 
 const searchFormEl = document.querySelector('.js-search-form');
 const galleryListEl = document.querySelector('.js-gallery');
-
 const loadMoreBtnEl = document.querySelector('.js-load-more');
 
-const unsplashAPI = new UnsplashAPI();
+const unsplashApi = new UnsplashAPI();
 
-const handleSearchPhotos = event => {
+const handleSearchFormSubmit = event => {
   event.preventDefault();
 
-  const searchQuery = event.target.elements['user-search-query'].value.trim();
+  const searchQuery = event.currentTarget.elements['user-search-query'].value;
+  unsplashApi.query = searchQuery;
+  unsplashApi.page = 1;
 
-  unsplashAPI.query = searchQuery;
-
-  unsplashAPI
+  unsplashApi
     .fetchPhotos()
     .then(data => {
       if (!data.results.length) {
-        throw new Error();
-      }
-
-      galleryListEl.innerHTML = createGalleryCards(data.results);
-
-      if (data.total_pages === unsplashAPI.page) {
+        console.log('Images not found!');
         return;
       }
 
+      galleryListEl.innerHTML = createGalleryCards(data.results);
       loadMoreBtnEl.classList.remove('is-hidden');
     })
-    .catch(() => {
-      loadMoreBtnEl.classList.add('is-hidden');
-      galleryListEl.textContent = 'Images not found';
+    .catch(err => {
+      console.log(err);
     });
 };
 
 const handleLoadMoreBtnClick = () => {
-  unsplashAPI.page += 1;
+  unsplashApi.page += 1;
 
-  unsplashAPI.fetchPhotos().then(data => {
-    if (unsplashAPI.page === data.total_pages) {
-      loadMoreBtnEl.classList.add('is-hidden');
-    }
+  unsplashApi
+    .fetchPhotos()
+    .then(data => {
+      console.log(data);
+      if (unsplashApi.page === data.total_pages) {
+        loadMoreBtnEl.classList.add('is-hidden');
+      }
 
-    galleryListEl.insertAdjacentHTML(
-      'beforeend',
-      createGalleryCards(data.results)
-    );
-  });
+      galleryListEl.insertAdjacentHTML(
+        'beforeend',
+        createGalleryCards(data.results)
+      );
+    })
+    .catch(err => {
+      console.log(err);
+    });
 };
 
-searchFormEl.addEventListener('submit', handleSearchPhotos);
-
+searchFormEl.addEventListener('submit', handleSearchFormSubmit);
 loadMoreBtnEl.addEventListener('click', handleLoadMoreBtnClick);
